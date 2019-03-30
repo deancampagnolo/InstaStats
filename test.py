9dcdbe2064a5da82ffefda673ccbb191dadb118c
Curtis <cantonid@ucsc.edu>
2019-03-30 03:24:17 -0700

    h2

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-print("hello world")
-print("FTP")
-print("FTP teu")
+import selenium
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+    #main function
+    n = "https://hypeauditor.com/top-instagram/?p=1"
+    page = main.get
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
