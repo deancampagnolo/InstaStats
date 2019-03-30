2f117ba1f6da2a0a49d0cf75fcb26e8f1b796a73
Curtis <cantonid@ucsc.edu>
2019-03-30 05:31:06 -0700

    day1

--- test.py
+++ test.py
@@ -5,10 +5,19 @@ import requests
 
 def main():
     #main function
-    n = "https://hypeauditor.com/top-instagram/?p=1"
-    page = main.get
+    n = "https://socialblade.com/instagram/top/500/followers"
 
+    page = requests.get(n)
+    print(page.status_code)
 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    a_list = (soup.find_all('a'))
+    i = 0
+    for item in a_list:
+        print(item)
+        print()
+        print(i)
+        i +=1
 
 
 
